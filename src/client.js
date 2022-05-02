3f5e64f629759524e463911af98f80ad1f0bc047
139Vaibhav <vaibhavkumarsingh139@gmail.com>
2022-05-02 06:47:54 +0530

    updated the client files

--- src/client.js
+++ src/client.js
@@ -1,6 +1,8 @@
 import * as contentful from 'contentful';
 
 export const client = contentful.createClient({
-    space: process.env.REACT_APP_SPACE_ID,
-    accessToken: process.env.REACT_APP_ACCESS_TOKEN
+    // space: process.env.REACT_APP_SPACE_ID,
+    // accessToken: process.env.REACT_APP_ACCESS_TOKEN
+    space: "k7qfzlbxq4u0",
+    accessToken: "rfi9BfbfxswACuc7wmbj8z-8ztzaDLqmyZ3Kv_Z-g0s"
 })
